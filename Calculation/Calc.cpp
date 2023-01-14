fa09b32013c744a13550c3e1b2912327006456e2
Nusi <okunev_d@list.ru>
2023-01-14 17:31:21 +0300

    Небольшое добавление(доработка функции переводящей строку в обратную польскую запись)

--- Calculation/Calc.cpp
+++ Calculation/Calc.cpp
@@ -25,8 +25,79 @@
 			inputStack(mass[i]);
 			out[coll] += number;
 			coll += 1;
-			out[coll] = outSign();
+			while (m != 0)
+			{
+				out[coll] += outSign();
+				coll += 1;
+			}
 			number = "";
 		}
 	}
+	emptyStack();
+	while (m != 0)
+	{
+		out[coll] += outSign();
+		coll += 1;
+		signs.pop();
+	}
+	delete[] out;
 }
+ void Calc::inputStack(char val)
+ {
+	 if ((val == '+' || val == '-') && ( signs.top() == '-' || signs.top() == '*' || signs.top() == '/' || signs.top() == '+'))
+	 {
+		 temporary[n] = signs.top();
+		 signs.pop();
+		 signs.push(val);
+		 n += 1;
+		 m = 1;
+	 }
+	 else if ((val == '*' || val == '/') && ( signs.top() == '*' || signs.top() == '/'))
+	 {
+		 temporary[n] = signs.top();
+		 signs.pop();
+		 signs.push(val);
+		 n += 1;
+		 m = 1;
+	 }
+	 else if (val == ')')
+	 {
+		 while (signs.top() != '(' && !signs.empty())
+		 {
+			 temporary[n] = signs.top();
+			 n += 1;
+			 signs.pop();
+		 }
+		 signs.pop();
+		 m = 1;
+	 }
+	 else if (val == '(')
+	 {
+		 signs.push(val);
+	 }
+ }
+ std::string Calc::outSign()
+ {
+	 std::string temporary;
+	 if (n != 0)
+	 {
+		 temporary = temporary[n];
+		 n -= 1;
+	 }
+	 else
+	 {
+		 temporary = temporary[n];
+		 m = 0;
+	 }
+	 return temporary;
+ }
+ void Calc::emptyStack()
+ {
+	 while (!signs.empty())
+	 {
+		 temporary[n] = signs.top();
+		 n += 1;
+		 signs.pop();
+	 }
+	 m = 1;
+ }
--- Calculation/Calc.h
+++ Calculation/Calc.h
@@ -7,8 +7,10 @@ class Calc
 public:
 	void input(std::string mass);
 	void inputStack(char val);
+	void emptyStack();
 	std::string outSign();
 private:
-	std::stack <std::string> values;
-	std::string temporary;
+	std::stack <char> signs;
+	std::string temporary[10];
+	int n = 0,m = 0;
 };
